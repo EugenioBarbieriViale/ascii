8a0b3aa17aeae661a708c993284e158d6f6b68a9
eu <82298389+EugenioBarbieriViale@users.noreply.github.com>
2024-02-24 23:22:53 +0100

    Update ascii_extended.py

--- ascii_extended.py
+++ ascii_extended.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-img = Image.open("input.jpeg")
+img = Image.open(str(input("Enter photos name: ")))
 pixelmap = img.load()
 
 list = ['$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', "'", '.', ' ']
