5e826d5792f73012074d72c5e4df2d848080eafd
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-08-03 15:50:16 +0200

    new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import pygame, sys
 
 img = Image.open("ex.png")
 
@@ -12,11 +13,12 @@ def img_resize(img):
     h = height // len(text)
 
     img = img.resize((w, h))
+    # img = img.resize((1000, 800))
 
     return img
 
 def get_lum(pixel):
-    # lum = (0.3 * p[0]) + (0.59 * p[1]) + (0.11 * p[2])
+    # return int((0.3 * pixel[0]) + (0.59 * pixel[1]) + (0.11 * pixel[2]))
     return ((pixel[0] + pixel[1] + pixel[2])//3) * (len(text)-1) // 255
 
 
@@ -25,11 +27,40 @@ img = img_resize(img)
 
 width, height = img.size
 
-for y in range(height):
-    for x in range(width):
-        p = img.getpixel((x, y))
+pygame.init()
+clock = pygame.time.Clock()
 
-        lum = get_lum(p)
-        print(text[lum], end="")
+X, Y = 1500, 1000
 
-    print("")
+screen = pygame.display.set_mode([X, Y])
+pygame.display.set_caption("ASCII")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((0,0,0))
+
+    font = pygame.font.SysFont("Comic Sans MS", len(text))
+
+    for y in range(height):
+        for x in range(width):
+            p = img.getpixel((x, y))
+
+            lum = get_lum(p)
+
+            display_text = font.render("{c}".format(c = text[lum]), 1, (255,255,255))
+            
+            dx = x*8
+            dy = y*9
+
+            screen.blit(display_text, (dx, dy))
+
+
+    pygame.display.flip()
+    clock.tick(30)
+    pygame.display.update()
+
+pygame.quit()
